30587dfa7c380fbaf11027eeab48758da4734d83
Felton Vaughn <felton@users.noreply.github.com>
2018-10-25 17:26:56 -0400

    BWSR-372: Add a helper to create mocked traits in unit tests

--- test/codeception/_support/Helper/Unit.php
+++ test/codeception/_support/Helper/Unit.php
@@ -6,5 +6,37 @@ namespace GraphQLClient\Tests\Helper;
 
 class Unit extends \Codeception\Module
 {
+    /**
+     * An instance of the unit test
+     *
+     * @var \Codeception\TestInterface
+     */
+    protected $unitTest;
 
+    /**
+     * Actions to peform before a unit test runs. In this case, keep an instance of the
+     * unit test to use in other methods here.
+     *
+     * @param \Codeception\TestInterface $test The unit test curently running
+     */
+    public function _before(\Codeception\TestInterface $test)
+    {
+        $this->unitTest = $test;
+    }
+
+    /**
+     * Mock a trait class
+     *
+     * @param string $className   Trait name
+     * @param array  $methodNames Methods to mock, if any
+     *
+     * @return object Mock object with trait `$className`
+     */
+    public function mockTrait($className = '', array $methodNames = [])
+    {
+        return $this->unitTest
+            ->getMockBuilder($className)
+            ->setMethods($methodNames)
+            ->getMockForTrait();
+    }
 }
